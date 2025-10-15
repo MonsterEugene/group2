6a3ba58fde5549895b6ef9fb7a5e78b16f14fc49
prestonchambers07 <10heartedlol@gmail.com>
2025-10-15 12:02:22 -1000

    started wordle

---
+++ app/games/wordle.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const Wordle  = (function({word, guess}) {  
+
+    let guess = props.guess; 
+    for(let i = 0; i < word.length; i++){
+        
+    }       
+    
+
+}
+
+) 
+
+
+
+export default app Wordle;
