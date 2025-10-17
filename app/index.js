2ba50f83a7b5c0b04966f19d430e1d6baf4e282c
Dalt <monstereugene07@gmail.com>
2025-10-17 11:22:36 -1000

    svg and expo functioning

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import { Link } from 'expo-router'; 
 import { StyleSheet, Text, View } from 'react-native';
+import BarGraph from '../components/graph';
 
 
 export default function App(){
+    return <View>
+        <Text>hello</Text>
+        <BarGraph/>
+    </View>
 } 
--- components/graph.js
+++ components/graph.js
@@ -1,7 +1,15 @@
-import { Svg, Line } from "react-native-svg";
+import { Svg, Line, Rect } from "react-native-svg";
 
 
-export default function Graph({data, }){
-
-
+export default function BarGraph({}){
+    return <Svg width={70} height={100}>
+        <Line x1={1} y1={100} x2={1} y2={0} strokeWidth={2} stroke={'black'} />
+        <Rect x={10} y={20} width={10} height={2} fill={"red"}/>
+        <Rect x={20} y={20} width={10} height={2} fill={"red"}/>
+        <Rect x={30} y={20} width={10} height={2} fill={"red"}/>
+        <Rect x={40} y={20} width={10} height={10} fill={"red"}/>
+        <Rect x={50} y={20} width={10} height={2} fill={"red"}/>
+        <Rect x={60} y={20} width={10} height={2} fill={"red"}/>
+        <Line x1={1} y1={0} x2={70} y2={0} strokeWidth={2} stroke={'black'} />
+    </Svg>
 }
