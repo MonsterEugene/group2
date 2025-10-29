2e270bab655ada55ac482ca42b593581d9299a10
hellotheretm <qwertygoofy.123@gmail.com>
2025-10-28 14:04:58 -1000

    wordle fix

--- app/games/wordle.js
+++ app/games/wordle.js
@@ -1,43 +1,17 @@
-import { Link } from 'expo-router';
-import { FlashList } from '@shopify/flash-list';
-import React, { useRef, useState } from 'react';
-// import useThemedTextStyle from '@site/src/hooks/useThemedTextStyle';
-import { Pressable, StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
-
-
-export default function Wordle() {
-    return (
-        <View style={styles.container}>
-            <Text>wordle</Text>
-        </View>
-    )
+export default function Wordle ({word, guess}) {  
+  //basic Wordle function to work around. 
+	let arr = [0,0,0,0,0]; 		
+    	let guess = props.guess; 
+    	for(let i = 0; i < word.length; i++){
+		if(word.includes(guess[i])){
+			if(world.includes(guess[i],i)){
+				arr[i] = 1;
+			}else{
+				arr[i] = 2; 	
+			}
+		}else{
+			arr[i] = 0; 
+		}
+    }
+	return arr;            	
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        // alignContent: '',
-    },
-    headerContainer: {
-        flex: 1,
-        backgroundColor: '#4e4e4eff',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headerText: {
-        fontSize: 32,
-        fontFamily: 'Arial',
-        color: '#ffffffff'
-    },
-    bodyContainer: {
-        flex: 5,
-        backgroundColor: '#6d6d6dff',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-    },
-})
