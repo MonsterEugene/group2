74e66b31d28b7aa41b5ecbe347fe702ced383bf6
swagfather808 <rkringor@gmail.com>
2025-10-31 11:27:55 -1000

    10/31
    Co-authored-by: hellotheretm <hellotheretm@users.noreply.github.com>

--- app/games/tic.js
+++ app/games/tic.js
@@ -4,56 +4,257 @@ import { useState } from 'react';
 export default function TicTacToe() {
   const [gameBoard, setGameBoard] = useState(["-", "-", "-", "-", "-", "-", "-", "-", "-"]);
   const [claimed, setClaimed] = useState([false, false, false, false, false, false, false, false, false]);
-  
+  const winnah = false;
+
+  const aiRng = () => {
+    let newClaimed = [...claimed]
+    let newGameBoard = [...gameBoard]
+    let o = Math.random() * 9
+    let claimed = newClaimed[o]
+    while (claimed) {
+      o = Math.random() * 9
+      claimed = newClaimed[o]
+    }
+    return o
+  }
+
   const move = (x) => {
-    gameBoard[x] = "X"
-    claimed[x] = true
+    let newClaimed = [...claimed]
+    let newGameBoard = [...gameBoard]
+    newGameBoard[x] = "X"
+    newClaimed[x] = true
+    setClaimed(newClaimed)
+    setGameBoard(newGameBoard)
+
+    const o = aiRng()
+    let aiClaimed = [...claimed]
+    let aiGameBoard = [...gameBoard]
+    aiGameBoard[o] = "O"
+    aiClaimed[o] = true
+    setClaimed(aiClaimed)
+    setGameBoard(aiGameBoard)
   }
   
+  const gameWin = () => {
+    for(let i = "X"; true; i = "O"){
+      if(gameBoard[0] == i && gameBoard[1] == i && gameBoard[2] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[3] == i && gameBoard[4] == i && gameBoard[5] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[6] == i && gameBoard[7] == i && gameBoard[8] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[0] == i && gameBoard[3] == i && gameBoard[6] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[1] == i && gameBoard[4] == i && gameBoard[7] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[2] == i && gameBoard[5] == i && gameBoard[8] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[0] == i && gameBoard[4] == i && gameBoard[8] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+      else if(gameBoard[2] == i && gameBoard[4] == i && gameBoard[6] == i){
+        if(i == "X"){
+          winnah = 1
+          break
+        }
+        else{
+          winnah = 2
+          break
+        }
+      }
+    }
+  }
 
   return(
     <View style = {styles.container}>
-      <View style={{ ...styles.press, marginRight: 285, marginBottom: 285}}> {/*1*/}
-        {claimed[0] ? (
+      if(winnah){
+        <Text style={styles.win}>YOU WIN!</Text>
+      }
+      <Image 
+        source={require('../../assets/grid.png')} 
+        style={styles.image} 
+      />
+      <Pressable
+        onPress={
+          () => {move(0)}
+        }
+        style={{ ...styles.press, marginRight: 285, marginBottom: 285}}
+      >
+        {claimed[0] && (
           <Image 
             source={require('../../assets/X.png')} 
             style={{width: 100, height: 100}} 
           />
         )
-        : (
-          <Pressable onPress={
-            () => move(0)
-          }/>
-        )}
-      </View>
-      <View style={{ ...styles.press, marginBottom: 285}}> {/*2*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginBottom: 285, marginLeft: 285}}> {/*3*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginRight: 285}}> {/*4*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press}}> {/*5*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginLeft: 285}}> {/*6*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginTop: 285, marginRight: 285}}> {/*7*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginTop: 285}}> {/*8*/}
-        <Pressable onPress={move}/>
-      </View>
-      <View style={{ ...styles.press, marginTop: 285, marginLeft: 285}}> {/*9*/}
-        <Pressable onPress={move}/>
-      </View>
-      <Image 
-        source={require('../../assets/grid.png')} 
-        style={styles.image} 
-      />
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(1)}
+        }
+        style={{ ...styles.press, marginBottom: 285}}
+      >
+        {claimed[1] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(2)}
+        }
+        style={{ ...styles.press, marginLeft: 285, marginBottom: 285}}
+      >
+        {claimed[2] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(3)}
+        }
+        style={{ ...styles.press, marginRight: 285}}
+      >
+        {claimed[3] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(4)}
+        }
+        style={{ ...styles.press}}
+      >
+        {claimed[4] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(5)}
+        }
+        style={{ ...styles.press, marginLeft: 285}}
+      >
+        {claimed[5] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(6)}
+        }
+        style={{ ...styles.press, marginRight: 285, marginTop: 285}}
+      >
+        {claimed[6] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(7)}
+        }
+        style={{ ...styles.press, marginTop: 285}}
+      >
+        {claimed[7] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
+      <Pressable
+        onPress={
+          () => {move(8)}
+        }
+        style={{ ...styles.press, marginLeft: 285, marginTop: 285}}
+      >
+        {claimed[8] && (
+          <Image 
+            source={require('../../assets/X.png')} 
+            style={{width: 100, height: 100}} 
+          />
+        )
+        } 
+      </Pressable>
     </View>
   )
   
@@ -67,9 +268,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   press: {
-    backgroundColor: "red",
     width: 100,
     height: 100,
+    position: 'absolute'
+  },
+  win: {
+    height: 500,
+    width: 100,
     position: 'absolute'
   }
 });
