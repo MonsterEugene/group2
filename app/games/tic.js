169dcbcfe42ec93cba351c6c7d5380bba6a4e939
swagfather808 <rkringor@gmail.com>
2025-11-07 10:27:27 -1000

    tic finished - no graph, no style

--- app/games/tic.js
+++ app/games/tic.js
@@ -1,19 +1,19 @@
-import { StyleSheet, Text, View, Pressable, Image, ScrollView } from 'react-native';
-import { useState } from 'react'; 
+import { StyleSheet, Text, View, Pressable, Image, ScrollView, Button } from 'react-native';
+import { useState } from 'react';
 import BarGraph from "../../components/graph.js"
 
 export default function TicTacToe() {
   const [gameBoard, setGameBoard] = useState(["-", "-", "-", "-", "-", "-", "-", "-", "-"]);
   const [claimed, setClaimed] = useState([false, false, false, false, false, false, false, false, false]);
   const [winnah, setWinnah] = useState(null);
-  const [wins, setWins] = useState([0, 0])
+  const [wins, setWins] = useState([0, 0]) //[player, computer]
 
   const aiRng = (currentClaimed) => {
     // Collect all available indices
     const availableIndices = currentClaimed
       .map((isClaimed, index) => isClaimed ? null : index)
       .filter(index => index !== null);
-      
+
     if (availableIndices.length === 0) {
       return null; // No available moves
     }
@@ -34,10 +34,20 @@ export default function TicTacToe() {
       const [a, b, c] = lines[i];
       if (board[a] !== '-' && board[a] === board[b] && board[a] === board[c]) {
         // Return 1 for X win, 2 for O win
-        return board[a] === 'X' ? 1 : 2; 
+        if(board[a] == 'X'){
+          let winsPlace = [...wins]
+          winsPlace[0] += 1 //Add 1 to player count
+          setWins(winsPlace)
+          return 1
+        } else {
+          let winsPlace = [...wins]
+          winsPlace[1] += 1 //Add 1 to robot count
+          setWins(winsPlace)
+          return 2
+        }
       }
     }
-    
+
     // Check for draw if no winner and all cells are claimed
     if (board.every(cell => cell !== '-')) {
       return 0; // Draw
@@ -45,6 +55,12 @@ export default function TicTacToe() {
 
     return null; // Game ongoing
   };
+
+  const reload = () => {
+    setGameBoard(["-", "-", "-", "-", "-", "-", "-", "-", "-"])
+    setClaimed([false, false, false, false, false, false, false, false, false])
+    setWinnah(null)
+  }
 
   const move = (x) => {
     // Only allow move if game is ongoing and cell is unclaimed
@@ -54,11 +70,11 @@ export default function TicTacToe() {
 
     let updatedGameBoard = [...gameBoard];
     let updatedClaimed = [...claimed];
-    
+
     // Player X move
     updatedGameBoard[x] = "X";
     updatedClaimed[x] = true;
-    
+
     // Check if X won immediately
     let winnerStatus = checkWinCondition(updatedGameBoard);
     if (winnerStatus !== null) {
@@ -70,18 +86,18 @@ export default function TicTacToe() {
 
     // AI O move (make sure we use the most recent board/claimed state)
     const o = aiRng(updatedClaimed);
-    
+
     if (o !== null) {
-        updatedGameBoard[o] = "O";
-        updatedClaimed[o] = true;
-        winnerStatus = checkWinCondition(updatedGameBoard);
+      updatedGameBoard[o] = "O";
+      updatedClaimed[o] = true;
+      winnerStatus = checkWinCondition(updatedGameBoard);
     }
-    
+
     setGameBoard(updatedGameBoard);
     setClaimed(updatedClaimed);
-    
+
     if (winnerStatus !== null) {
-        setWinnah(winnerStatus);
+      setWinnah(winnerStatus);
     }
   };
   const getWinnerText = () => {
@@ -98,255 +114,263 @@ export default function TicTacToe() {
       style={{
         ...styles.press,
         // Simplified layout offsets (you may need to tweak these based on your grid image size)
-        top: Math.floor(index / 3) * 120 - 120, 
+        top: Math.floor(index / 3) * 120 - 120,
         left: (index % 3) * 120 - 120,
       }}
       disabled={claimed[index] || winnah !== null} // Disable presses if claimed or game over
     >
       {claimed[index] && gameBoard[index] === 'X' && (
-        <Image 
+        <Image
           source={require('../../assets/X.png')} // Make sure this path is correct
-          style={{width: 100, height: 100}} 
+          style={{ width: 100, height: 100 }}
         />
       )}
       {claimed[index] && gameBoard[index] === 'O' && (
-        <Image 
+        <Image
           source={require('../../assets/O.png')} // !!! You NEED this asset for the AI to show up !!!
-          style={{width: 100, height: 100}} 
+          style={{ width: 100, height: 100 }}
         />
       )}
     </Pressable>
   );
-  
 
-//   return(
-//     <View style={styles.container}>
-//       {/* This is how you correctly render conditionally in JSX */}
-//       {getWinnerText() && (
-//         <Text style={styles.win}>{getWinnerText()}</Text>
-//       )}
-      
-//       <Image 
-//         source={require('../../assets/grid.png')} // Make sure this path is correct
-//         style={styles.image} 
-//       />
-      
-//       {/* Use a loop or map to generate the 9 cells instead of repeating them */}
-//       {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(index => (
-//         <Cell key={index} index={index} onPress={() => move(index)} />
-//       ))}
-//     </View>
-//   );
-// }
-  
 
-  return(
-    <View style = {styles.container}>
-      {/* This is still invalid syntax and won't work in React Native: */}
-        {getWinnerText() && (
-          <Text style={styles.winText}>{getWinnerText()}</Text>
+  //   return(
+  //     <View style={styles.container}>
+  //       {/* This is how you correctly render conditionally in JSX */}
+  //       {getWinnerText() && (
+  //         <Text style={styles.win}>{getWinnerText()}</Text>
+  //       )}
+
+  //       <Image 
+  //         source={require('../../assets/grid.png')} // Make sure this path is correct
+  //         style={styles.image} 
+  //       />
+
+  //       {/* Use a loop or map to generate the 9 cells instead of repeating them */}
+  //       {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(index => (
+  //         <Cell key={index} index={index} onPress={() => move(index)} />
+  //       ))}
+  //     </View>
+  //   );
+  // }
+
+
+  return (
+    <View style={styles.container}>
+      <Button 
+        onPress={
+          () => { reload() }
+        }
+        title="Play Again"
+      />
+      <Text>
+        {wins[0]} - {wins[1]}
+      </Text>
+      {getWinnerText() && (
+        <Text style={styles.winText}>{getWinnerText()}</Text>
+      )}
+
+      <Image
+        source={require('../../assets/grid.png')}
+        style={styles.image}
+      />
+
+      {/* --- INDEX 0 FIX --- */}
+      <Pressable
+        onPress={
+          () => { move(0) }
+        }
+        style={{ ...styles.press, marginRight: 285, marginBottom: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[0] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
         )}
-
-        <Image 
-          source={require('../../assets/grid.png')} 
-          style={styles.image} 
-        />
-        
-        {/* --- INDEX 0 FIX --- */}
-        <Pressable
-          onPress={
-            () => {move(0)}
-          }
-          style={{ ...styles.press, marginRight: 285, marginBottom: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[0] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[0] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(1)}
-          }
-          style={{ ...styles.press, marginBottom: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[1] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[1] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(2)}
-          }
-          style={{ ...styles.press, marginLeft: 285, marginBottom: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[2] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[2] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(3)}
-          }
-          style={{ ...styles.press, marginRight: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[3] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[3] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )} 
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(4)}
-          }
-          style={{ ...styles.press}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[4] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[4] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(5)}
-          }
-          style={{ ...styles.press, marginLeft: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[5] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[5] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )} 
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(6)}
-          }
-          style={{ ...styles.press, marginRight: 285, marginTop: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[6] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[6] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(7)}
-          }
-          style={{ ...styles.press, marginTop: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[7] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[7] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )} 
-        </Pressable>
-        <Pressable
-          onPress={
-            () => {move(8)}
-          }
-          style={{ ...styles.press, marginLeft: 285, marginTop: 285}}
-        >
-          {/* We use gameBoard[0] instead of claimed[0] */}
-          {gameBoard[8] === 'X' && (
-            <Image 
-              source={require('../../assets/X.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-          {/* ADDED: A check for 'O' */}
-          {gameBoard[8] === 'O' && (
-            <Image 
-              source={require('../../assets/O.png')} 
-              style={{width: 100, height: 100}} 
-            />
-          )}
-        </Pressable> 
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[0] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(1) }
+        }
+        style={{ ...styles.press, marginBottom: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[1] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[1] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(2) }
+        }
+        style={{ ...styles.press, marginLeft: 285, marginBottom: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[2] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[2] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(3) }
+        }
+        style={{ ...styles.press, marginRight: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[3] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[3] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(4) }
+        }
+        style={{ ...styles.press }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[4] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[4] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(5) }
+        }
+        style={{ ...styles.press, marginLeft: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[5] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[5] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(6) }
+        }
+        style={{ ...styles.press, marginRight: 285, marginTop: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[6] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[6] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(7) }
+        }
+        style={{ ...styles.press, marginTop: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[7] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[7] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
+      <Pressable
+        onPress={
+          () => { move(8) }
+        }
+        style={{ ...styles.press, marginLeft: 285, marginTop: 285 }}
+      >
+        {/* We use gameBoard[0] instead of claimed[0] */}
+        {gameBoard[8] === 'X' && (
+          <Image
+            source={require('../../assets/X.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        {/* ADDED: A check for 'O' */}
+        {gameBoard[8] === 'O' && (
+          <Image
+            source={require('../../assets/O.png')}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </Pressable>
 
       {/* <View>
         <BarGraph size={400} d={[{key: 0, amo: wins[0]}, {key: 1, amo: wins[1]}]}> </BarGraph>
       </View> */}
-      
+
     </View>
   )
 }
@@ -365,6 +389,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'pink',
     flexGrow: 1,
   },
+  reload: {
+    justifyContent: 'center',
+    marginLeft: 400,
+  },
   press: {
     width: 100,
     height: 100,
@@ -374,6 +402,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'absolute',
     fontSize: 100,
-    marginBottom: 550
+    marginBottom: 600
   },
 });
